4b1492512f948749cc08cad99049f99a73bba04f
parker.loomis1 <parker.loomis1@gmail.com>
2021-06-16 14:30:05 -0400

    Update run-docker.sh

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Check if autosurvey-network network exists
-if [ -z "$(docker network ls -q -f name=autosurvey-network)" ]; then
+if [ -z "$(docker network ls -q -f name=dmv-network)" ]; then
     docker network create autosurvey-network
 fi
 
 # rm gateway-service container if it exists
-if [ -n "$(docker container ls -aqf name=gateway-service)" ]; then
+if [ -n "$(docker container ls -aqf name=dmv-service)" ]; then
     docker container stop gateway-service
     docker container rm gateway-service
 fi
 
 #start gateway-service container
-docker container run -d --name gateway-service --network autosurvey-network \
-    -p 80:8080 -e EUREKA_URL autosurvey/gateway-service
+docker container run -d --name gateway-service --network dmv-network \
+    -p 80:8080 -e EUREKA_URL dmv/gateway-service
 
 #Changed permission
